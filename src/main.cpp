90f7677d0540253d9998cbfe02df96c986fe4d7f
lina <alexis.lina@epitech.ei>
2016-04-12 10:05:49 +0200

    test regex dossier tmp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 // Created by lina on 11/04/16.
 //
 
+#include <iostream>
+
 int     main(int ac, char **av)
 {
   return (0);
---
+++ tmp/test_regex.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+int	main(void)
+{
+  std::string input;
+
+  std::regex	integer("(\\+|-)?[[:digit:]]+");
+  while (true)
+    {
+      std::cout << "Give me an integer !" << std::endl;
+      std::cin >> input;
+      if (!std::cin)
+	break;
+      if (input == "q")
+	break;
+      if (regex_match(input, integer))
+	std::cout << "integer" << std::endl;
+      else
+	std::cout << "Invalid input" << std::endl;
+    }
+}
