d0d6c33ac4c745ed9ff33f1e2f731a31c9097e44
schmou <contact@klausto.net>
2016-04-21 17:58:43 +0200

    cipher qui merde sur le xor

--- bonus/cipher.c
+++ bonus/cipher.c
@@ -5,7 +5,7 @@
 ** Login   <schmou@epitech.net>
 **
 ** Started on  Tue Apr 19 14:35:53 2016 schmou_a
-** Last update Thu Apr 21 16:46:01 2016 schmou_a
+** Last update Thu Apr 21 17:52:22 2016 schmou_a
 */
 
 #include <sys/types.h>
@@ -14,21 +14,27 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int	disp_usage()
+{
+  dprintf(STDERR_FILENO,
+	  "usage: ./cipher path_to_file encryption_type(xor | caesar) key(1 byte for caesar, 2 for xor)\n");
+  return (1);
+}
 
 int	main(int ac, char **av)
 {
   int	fdin;
-  int	fdout;
   char	buff[500001];
+  int	key;
+  int	len;
 
+  if (ac != 4)
+    return (disp_usage());
   if ((fdin = open(av[1], O_RDONLY)) == -1)
     {
       dprintf(STDERR_FILENO,"%m\n");
-      return (-1);
-    }
-  if ((fdout = open("ciphered", O_RDWR)) == -1)
-    {
-      dprintf(STDERR_FILENO, "%m\n");
       return (-1);
     }
   memset(buff, '\0', 500000);
@@ -37,9 +43,25 @@ int	main(int ac, char **av)
       dprintf(STDERR_FILENO, "%m\n");
       return (-1);
     }
-  for(int i = 0; i < strlen(buff); i++)
-      buff[i] = buff[i] + 0xe2;
-  write(fdout, buff, strlen(buff));
   close(fdin);
-  close(fdout);
+  key = atoi(av[3]);
+  len = strlen(buff);
+  if (!strcmp(av[2], "xor"))
+    {
+      for (int i = 0; i < len; i++)
+	{
+	  buff[i] = buff[i] ^ (key >> 8);
+	  buff[i + 1] = buff[i + 1] ^ key;
+	  i++;
+	}
+    }
+  else if (!strcmp(av[2], "caesar"))
+    {
+      for (int i = 0; i < len; i++)
+	buff[i] += key;
+    }
+  else
+    return (disp_usage());
+  write(1, buff, len);
+  return (0);
 }
