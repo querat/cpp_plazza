c0520eeeaf1422b80fdfd71699344c72381af340
lina_a <alexis.lina@epitech.eu>
2016-04-21 17:04:18 +0200

    Regex faite au max pour  un max d'opti

--- bonus/cipher.c
+++ bonus/cipher.c
@@ -1,9 +1,9 @@
 /*
 ** cipher.c for  in /home/schmou/cpp
-** 
+**
 ** Made by schmou_a
 ** Login   <schmou@epitech.net>
-** 
+**
 ** Started on  Tue Apr 19 14:35:53 2016 schmou_a
 ** Last update Thu Apr 21 16:46:01 2016 schmou_a
 */
@@ -12,6 +12,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
@@ -19,16 +21,24 @@ int	main(int ac, char **av)
   int	fdout;
   char	buff[500001];
 
-  fdin = open(av[1], O_RDONLY);
-  fdout = open("ciphered", O_RDWR);
-
-  memset(buff, '\0', 500000);
-  read(fdin, buff, 500000);
-  for(int i = 0; i < strlen(buff); i++)
+  if ((fdin = open(av[1], O_RDONLY)) == -1)
     {
-      
-      buff[i] = buff[i] + 0xe2;
+      dprintf(STDERR_FILENO,"%m\n");
+      return (-1);
     }
+  if ((fdout = open("ciphered", O_RDWR)) == -1)
+    {
+      dprintf(STDERR_FILENO, "%m\n");
+      return (-1);
+    }
+  memset(buff, '\0', 500000);
+  if ((read(fdin, buff, 500000)) < 0)
+    {
+      dprintf(STDERR_FILENO, "%m\n");
+      return (-1);
+    }
+  for(int i = 0; i < strlen(buff); i++)
+      buff[i] = buff[i] + 0xe2;
   write(fdout, buff, strlen(buff));
   close(fdin);
   close(fdout);
