1b9bd611ac488e73e248b70844ea4a0f17c36773
lina_a <alexis.lina@epitech.eu>
2016-04-23 11:12:37 +0200

    Rajout es regex dans ecxtract data ainsi que de l'open file -> tout fonctionne

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,48 @@
 // Login   <querat_g@epitech.net>
 //
 // Started on  Wed Apr 13 22:43:06 2016 querat_g
-// Last update Fri Apr 22 16:58:12 2016 querat_g
+// Last update Fri Apr 22 13:53:34 2016 schmou_a
 //
 
 #include <stdexcept>
-
-#include <sys/wait.h>
-
 #include <iostream>
 #include "Plazza.hh"
+#include "DataCollector.hh"
+
+// int     elFork(char **av)
+// {
+//   NamedPipe	pipe("myFifo");
+//   int		child_pid = 0;
+
+//   child_pid = fork();
+//   if (child_pid == -1)
+//     {
+//       std::cerr << "fork has ded" << std::endl;
+//       return (1);
+//     }
+//   if (child_pid) // parent
+//     {
+//       std::string a = pipe.readFrom();
+//       a = pipe.readFrom();
+//       std::cout << "reading " << a << std::endl;
+//       wait(0);
+//     }
+//   else
+//     {
+//       pipe.writeTo(av[1]);
+//     }
+
+//   return (true);
+// }
 
 int     main(int ac, char **av)
 {
   Plazza::Main  *plazza = nullptr;
+  t_FileActionPair      fileInfo;
+  fileInfo = std::make_pair("/home/lina/rendu/B4/C++/cpp_plazza/ciphered2", Plazza::Action::Type::PHONE_NUMBER);
+  dataCollector         data;
+
+  std::cout << data.extract_data(fileInfo);
 
   if (ac != 2)
     {
@@ -25,7 +54,8 @@ int     main(int ac, char **av)
       return (1);
     }
 
-  try {
+  try
+  {
     plazza = new Plazza::Main(av[1]);
   }
   catch (std::invalid_argument const & exception) {
@@ -33,7 +63,7 @@ int     main(int ac, char **av)
     return (1);
   }
 
-  int   exitStatus = plazza->mainLoop();
-  plazza->killProcesses();
-  return (exitStatus);
+  // plazza->fork();
+
+  return (0);
 }
